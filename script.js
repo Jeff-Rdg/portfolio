f84960089115685948d18ba18961d8c26123d2b4
Jefferson Rodrigues <jsilvardg@outlook.com>
2021-11-15 22:33:57 -0300

    :package: orçamento
    Atualizando

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function(){
+    const prazo = document.querySelector("#prazo").value;
+    document.querySelector("label[for=prazo]").innerHTML = prazo;
+ atualizarPreco()})
+    
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked;
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+
+    let preco = qtde * 100;
+    if (temJS){
+        preco *= 1.10
+    }
+    if(incluiLayout){
+        preco = preco + 500
+
+    }
+    document.querySelector("#preco").innerHTML = "R$ " + preco.toFixed(2)
+
+}
